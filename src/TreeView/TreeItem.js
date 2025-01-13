5a8bf747075de88f3e2cbae312fa8aabc8cce2f4
mdzobaidurislam <freelancerjami@gmail.com>
2025-01-13 21:45:07 +0600

    avibale

--- src/TreeView/TreeItem.js
+++ src/TreeView/TreeItem.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const TreeItem = ({isSaveData=false, item, level = 0, onUpdate }) => {
+const TreeItem = ({ isSaveData = false, item, level = 0, onUpdate }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [itemData, setItemData] = useState(item);
 
@@ -14,7 +14,7 @@ const TreeItem = ({isSaveData=false, item, level = 0, onUpdate }) => {
   const itemClass = `tree-item ${isNode ? "node" : item.isEditable ? "editable" : "non-editable"}`;
   const backgroundColor = isNode ? "lightblue" : item.isEditable ? "white" : "lightgray";
 
-  console.log("isSaveData",isSaveData)
+  console.log("item", item)
   return (
     <div>
       <div
@@ -33,51 +33,51 @@ const TreeItem = ({isSaveData=false, item, level = 0, onUpdate }) => {
         <span className="item-label">{itemData.label}:</span>
 
         {!isNode && (
-  item.isEditable ? (
-    itemData.type === "checkbox" ? (
-      <input
-        disabled={isSaveData}
-        type="checkbox"
-        checked={Boolean(itemData.value)}
-        onChange={(e) => handleChange("value", e.target.checked)}
-        style={{ marginLeft: "10px" }}
-      />
-    ) : itemData.type === "select" ? (
-      <select
-      disabled={isSaveData}
-        value={itemData.value}
-        onChange={(e) => handleChange("value", e.target.value)}
-        style={{
-          backgroundColor: item.isEditable ? "white" : "lightgray",
-          marginLeft: "10px",
-        }}
-      >
-        {itemData.options?.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    ) : (
-      <input
-        disabled={isSaveData}
-        type={itemData.type}
-        value={itemData.value}
-        onChange={(e) => handleChange("value", e.target.value)}
-        style={{
-          backgroundColor: item.isEditable ? "white" : "lightgray",
-        }}
-      />
-    )
-  ) : (
-    <span
-      className="item-value"
-      style={{ backgroundColor: "lightgray" }}
-    >
-      {itemData.value?.toString()}
-    </span>
-  )
-)}
+          item.isEditable ? (
+            itemData.type === "checkbox" ? (
+              <input
+                disabled={isSaveData}
+                type={itemData.type}
+                checked={Boolean(itemData.value)}
+                onChange={(e) => handleChange("value", e.target.checked)}
+                style={{ marginLeft: "10px" }}
+              />
+            ) : itemData.type === "select" ? (
+              <select
+                disabled={isSaveData}
+                value={itemData.value}
+                onChange={(e) => handleChange("value", e.target.value)}
+                style={{
+                  backgroundColor: item.isEditable ? "white" : "lightgray",
+                  marginLeft: "10px",
+                }}
+              >
+                {itemData.options?.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            ) : (
+              <input
+                disabled={isSaveData}
+                type={itemData.type}
+                value={itemData.value}
+                onChange={(e) => handleChange("value", e.target.value)}
+                style={{
+                  backgroundColor: item.isEditable ? "white" : "lightgray",
+                }}
+              />
+            )
+          ) : (
+            <span
+              className="item-value"
+              style={{ backgroundColor: "lightgray" }}
+            >
+              {itemData.value?.toString()}
+            </span>
+          )
+        )}
 
       </div>
 
